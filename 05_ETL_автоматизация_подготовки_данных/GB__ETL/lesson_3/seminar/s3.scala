af9ca1d16aee62a69d13e99ea0af752b4f7abba4
Константин Ярохно <kostia@MacBook-Air-Konstantin.local>
2024-12-09 17:06:25 +0300

    Сommitted on пн 09.12.2024 05:06:25 PM MSK

---
+++ 05_ETL_автоматизация_подготовки_данных/GB__ETL/lesson_3/seminar/s3.scala
@@ -0,0 +1,34 @@
+/*
+chcp 65001 && spark-shell -i \spark\s3.scala --conf "spark.driver.extraJavaOptions=-Dfile.encoding=utf-8"
+*/
+import org.apache.spark.internal.Logging
+import org.apache.spark.sql.functions.{col, collect_list, concat_ws}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.expressions.Window
+val t1 = System.currentTimeMillis()
+if(1==1){
+var df1 = spark.read.format("com.crealytics.spark.excel")
+        .option("sheetName", "Sheet1")
+        .option("useHeader", "false")
+        .option("treatEmptyValuesAsNulls", "false")
+        .option("inferSchema", "true").option("addColorColumns", "true")
+		.option("usePlainNumberFormat","true")
+        .option("startColumn", 0)
+        .option("endColumn", 99)
+        .option("timestampFormat", "MM-dd-yyyy HH:mm:ss")
+        .option("maxRowsInMemory", 20)
+        .option("excerptSize", 10)
+        .option("header", "true")
+        .format("excel")
+        .load("/spark/s3.xlsx")
+		df1.write.format("jdbc").option("url","jdbc:mysql://localhost:3306/spark?user=root&password=root")
+        .option("driver", "com.mysql.cj.jdbc.Driver").option("dbtable", "s3")
+        .mode("overwrite").save()
+}
+val s0 = (System.currentTimeMillis() - t1)/1000
+val s = s0 % 60
+val m = (s0/60) % 60
+val h = (s0/60/60) % 24
+println("%02d:%02d:%02d".format(h, m, s))
+System.exit(0)
--- 05_ETL_автоматизация_подготовки_данных/readme.md
+++ 05_ETL_автоматизация_подготовки_данных/readme.md
@@ -1,2 +1,10 @@
-Запускаем PostgreSQL в Docker:
+Запускаем Spark в Docker:
+```bash
+docker run -d \
+  --name spark-master \
+  -p 7077:7077 \
+  -p 8080:8080 \
+  -v /Users/kostia/Programs:/var/lib/mydir \
+  bitnami/spark:latest \
+  ./bin/spark-class org.apache.spark.deploy.master.Master
 ```
