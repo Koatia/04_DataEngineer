bb5a0eb5920bfff865bb164d308cd376ca853e65
Константин Ярохно <kostia@MacBook-Air-Konstantin.local>
2024-12-12 11:11:25 +0300

    Сommitted on чт 12.12.2024 11:11:25 AM MSK

--- 05_ETL_автоматизация_подготовки_данных/1._Модели_данных_и_нормализация_таблиц._Схема_«звезда»/d1.scala
+++ 05_ETL_автоматизация_подготовки_данных/1._Модели_данных_и_нормализация_таблиц._Схема_«звезда»/d1.scala
@@ -1,119 +1,52 @@
 /*
-chcp 65001 && spark-shell -i /var/lib/mydir/d1.scala --conf "spark.driver.extraJavaOptions=-Dfile.encoding=utf-8"
+chcp 65001 && \
+spark-shell --packages com.crealytics:spark-excel_2.12:3.5.1_0.20.4,org.postgresql:postgresql:42.6.0 -i /var/lib/mydir/d1.scala --conf "spark.driver.extraJavaOptions=-Dfile.encoding=utf-8"
 */
-import org.apache.spark.internal.Logging
-import org.apache.spark.sql.functions.{col, collect_list, concat_ws}
-import org.apache.spark.sql.{DataFrame, SparkSession}
-val t1 = System.currentTimeMillis()
-if(1==1){
-		var df1 = spark.read.format("com.crealytics.spark.excel")
-        .option("sheetName", "Sheet1")
-        .option("useHeader", "false")
-        .option("treatEmptyValuesAsNulls", "false")
-        .option("inferSchema", "true").option("addColorColumns", "true")
-		.option("usePlainNumberFormat","true")
-        .option("startColumn", 0)
-        .option("endColumn", 99)
-        .option("timestampFormat", "MM-dd-yyyy HH:mm:ss")
-        .option("maxRowsInMemory", 20)
-        .option("excerptSize", 10)
-        .option("header", "true")
-        .format("excel")
-        .load("/var/lib/mydir/d1.xlsx")
-		df1.show()
-		
-		df1.filter(df1("Employee_ID").isNotNull).select("Employee_ID","Name","Job_Code","Job","City_code","Home_city")
-		.write.format("jdbc").option("url","jdbc:mysql://localhost:3306/spark?user=root&password=root")
-        .option("driver", "com.mysql.cj.jdbc.Driver").option("dbtable", "tabr")
-        .mode("overwrite").save()
 
-		import java.sql._;
-		def sqlexecute(sql: String) = {
-			var conn: Connection = null;
-			var stmt: Statement = null;
-			try {
-				Class.forName("com.mysql.cj.jdbc.Driver");
-				conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/spark?user=root&password=root");
-				stmt = conn.createStatement();
-				stmt.executeUpdate(sql);
-				println(sql+" complete");
-			} catch {
-				case e: Exception => println ("exception caught" + e);
-			}
-		}
-		sqlexecute("drop table spark.tabr1")
-		sqlexecute("drop table spark.tabr2")
-		sqlexecute("drop table spark.tabr3")
-		sqlexecute("drop table spark.tabr4")
-		sqlexecute("""
-		CREATE TABLE `tabr1` (
-			`City_code` INT(10) NOT NULL,
-			`Home_city` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
-			PRIMARY KEY (`City_code`) USING BTREE
-		)
-		COMMENT='Успешно!'
-		COLLATE='utf8mb4_0900_ai_ci'
-		ENGINE=InnoDB
-		""")
-		sqlexecute("""
-		CREATE TABLE `tabr2` (
-			`Job_Code` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_0900_ai_ci',
-			`Job` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
-			PRIMARY KEY (`Job_Code`) USING BTREE
-		)
-		COMMENT='Успешно!'
-		COLLATE='utf8mb4_0900_ai_ci'
-		ENGINE=InnoDB
-		;
-		""")
-		sqlexecute("""
-		CREATE TABLE `tabr3` (
-			`Employee_ID` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
-			`Name` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
-			`Job_Code` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_0900_ai_ci',
-			INDEX `Employee_ID` (`Employee_ID`, `Name`) USING BTREE,
-			INDEX `Job_Code` (`Job_Code`) USING BTREE
-		)
-		COMMENT='Успешно!'
-		COLLATE='utf8mb4_0900_ai_ci'
-		ENGINE=InnoDB
-		;
-		""")
-		sqlexecute("""
-		CREATE TABLE `tabr4` (
-			`Employee_ID` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
-			`Name` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
-			`City_code` INT(10) NOT NULL,
-			INDEX `Employee_ID` (`Employee_ID`, `Name`) USING BTREE,
-			INDEX `City_code` (`City_code`) USING BTREE
-		)
-		COMMENT='Успешно!'
-		COLLATE='utf8mb4_0900_ai_ci'
-		ENGINE=InnoDB
-		;
-		""")
-		df1.select("City_code","Home_city").distinct()
-		.write.format("jdbc").option("url","jdbc:mysql://localhost:3306/spark?user=root&password=root")
-		.option("driver", "com.mysql.cj.jdbc.Driver").option("dbtable", "tabr1")
-        .mode("append").save()
-		df1.select("Job_Code","Job").distinct()
-		.write.format("jdbc").option("url","jdbc:mysql://localhost:3306/spark?user=root&password=root")
-		.option("driver", "com.mysql.cj.jdbc.Driver").option("dbtable", "tabr2")
-		.mode("append").save()
-		df1.select("Employee_ID","Name","Job_Code").distinct()
-		.write.format("jdbc").option("url","jdbc:mysql://localhost:3306/spark?user=root&password=root")
-		.option("driver", "com.mysql.cj.jdbc.Driver").option("dbtable", "tabr3")
-		.mode("append").save()
-		df1.select("Employee_ID","Name","City_code").distinct()
-		.write.format("jdbc").option("url","jdbc:mysql://localhost:3306/spark?user=root&password=root")
-		.option("driver", "com.mysql.cj.jdbc.Driver").option("dbtable", "tabr4")
-        .mode("append").save()
-		
-	println("task 1")
+//import org.apache.spark.internal.Logging
+//import org.apache.spark.sql.functions.{col, collect_list, concat_ws}
+//import org.apache.spark.sql.{DataFrame, SparkSession}
+
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+// Настройки подключения к PostgreSQL
+val dbUrl = "jdbc:postgresql://postgres-db:5432/mydb" // Укажите хост, порт и имя базы данных
+val dbUser = "user"
+val dbPassword = "password"
+val dbDriver = "org.postgresql.Driver"
+
+// Читаем Excel
+val df1 = spark.read.format("com.crealytics.spark.excel")
+  .option("sheetName", "Лист1") // Укажите имя листа
+  .option("useHeader", "true") // Обязательно указать этот параметр
+  .option("header", "true") // Указывает, есть ли заголовки в первой строке
+  .option("treatEmptyValuesAsNulls", "false")
+  .option("inferSchema", "true")
+  .option("startColumn", 0)
+  .option("endColumn", 99)
+  .option("timestampFormat", "MM-dd-yyyy HH:mm:ss")
+  .load("/var/lib/mydir/d1.xlsx")
+
+df1.show()
+
+// Функция для записи DataFrame в PostgreSQL
+def writeToPostgres(df: DataFrame, tableName: String): Unit = {
+  df.write
+    .format("jdbc")
+    .option("url", dbUrl)
+    .option("dbtable", tableName)
+    .option("user", dbUser)
+    .option("password", dbPassword)
+    .option("driver", dbDriver)
+    .mode("append") // Можно использовать "overwrite" для перезаписи
+    .save()
 }
-val s0 = (System.currentTimeMillis() - t1)/1000
-val s = s0 % 60
-val m = (s0/60) % 60
-val h = (s0/60/60) % 24
-println("%02d:%02d:%02d".format(h, m, s))
+
+// Запись данных в таблицы PostgreSQL
+writeToPostgres(df1.select("City_code", "Home_city").distinct(), "tabr1")
+writeToPostgres(df1.select("Job_Code", "Job").distinct(), "tabr2")
+writeToPostgres(df1.select("Employee_ID", "Name", "Job_Code").distinct(), "tabr3")
+writeToPostgres(df1.select("Employee_ID", "Name", "City_code").distinct(), "tabr4")
+
+println("Данные успешно сохранены в PostgreSQL.")
 System.exit(0)
--- 05_ETL_автоматизация_подготовки_данных/readme.md
+++ 05_ETL_автоматизация_подготовки_данных/readme.md
@@ -1,4 +1,18 @@
+Запускаем PostgreSQL
+Используем стандартный образ PostgreSQL с открытым портом для внешнего подключения:
+
+```shell
+docker run -d \
+  --name postgres-db \
+  -e POSTGRES_USER=user \
+  -e POSTGRES_PASSWORD=password \
+  -e POSTGRES_DB=mydb \
+  -p 5432:5432 \
+  postgres:latest
+```
+
 Запускаем Spark в Docker:
+
 ```bash
 docker run -d \
   --name spark-master \
@@ -7,4 +21,61 @@ docker run -d \
   -v /Users/kostia/Programs:/var/lib/mydir \
   bitnami/spark:latest \
   ./bin/spark-class org.apache.spark.deploy.master.Master
+```
+
+
+Создаем новую сеть Docker:
+
+```bash
+docker network create spark-postgres-network
+```
+
+
+2. Перезапустите контейнеры и подключите их к новой сети
+
+Переподключите PostgreSQL и Spark к созданной сети:
+
+Подключение PostgreSQL:
+
+Если контейнер PostgreSQL уже запущен, отключите его и снова запустите, подключив к новой сети:
+
+docker network connect spark-postgres-network <имя_контейнера_postgres>
+
+Подключение Spark:
+
+Для контейнера Spark:
+
+docker network connect spark-postgres-network <имя_контейнера_spark>
+
+3. Убедитесь, что оба контейнера находятся в одной сети
+
+Проверьте, что оба контейнера находятся в одной сети:
+
+docker network inspect spark-postgres-network
+
+В выводе команды вы должны увидеть оба контейнера (PostgreSQL и Spark) под одним и тем же идентификатором сети.
+
+4. Проверьте подключение
+
+Выполните внутри контейнера Spark тестовый telnet на порт PostgreSQL:
+
+docker exec -it <имя_контейнера_spark> bash
+telnet postgres-db 5432
+
+Если соединение успешно, значит контейнеры теперь могут взаимодействовать.
+
+5. Проверьте URL JDBC
+
+Теперь в вашем скрипте Scala используйте имя сервиса PostgreSQL как хост:
+
+val jdbcUrl = "jdbc:postgresql://postgres-db:5432/mydb"
+
+Попробуйте снова выполнить ваш скрипт Spark. Если возникнут ошибки, поделитесь новыми логами, и мы их разберем.
+
+
+
+
+Запускаем скрипт с указанием библиотеки
+```bash
+spark-shell --packages com.crealytics:spark-excel_2.12:3.5.1_0.20.4 -i /var/lib/mydir/d1.scala --conf "spark.driver.extraJavaOptions=-Dfile.encoding=utf-8"
 ```
