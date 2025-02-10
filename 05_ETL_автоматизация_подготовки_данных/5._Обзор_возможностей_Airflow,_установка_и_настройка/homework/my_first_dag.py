f4eb1815e5ae622b4e1cd8ec72ca80a7ca683aee
Konstantin <yarohno@gmail.com>
2025-02-10 16:31:36 +0300

    Сommitted on пн 10.02.2025 04:31:36 PM MSK

--- 05_ETL_автоматизация_подготовки_данных/5._Обзор_возможностей_Airflow,_установка_и_настройка/homework/my_first_dag.py
+++ 05_ETL_автоматизация_подготовки_данных/5._Обзор_возможностей_Airflow,_установка_и_настройка/homework/my_first_dag.py
@@ -1,17 +1,31 @@
 from datetime import datetime
-
 from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python import PythonOperator
 
 
 def print_hello():
-    return 'Привет Мир, первый запуск Airflow DAG!'
+    return 'Привет, Мир! Первый запуск Airflow DAG!'
 
 
-dag = DAG('my_first_dag', description='Доброе пожаловать Илья, с вами DAG',
-          schedule_interval='0 12 * * *',
-          start_date=datetime(2017, 3, 20), catchup=False)
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2017, 3, 20),
+    'retries': 1,
+}
 
-hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
+dag = DAG(
+    dag_id='my_first_dag',
+    description='Добро пожаловать, Konstantin! С вами DAG',
+    schedule_interval='0 12 * * *',
+    catchup=False,
+    default_args=default_args
+)
+
+hello_operator = PythonOperator(
+    task_id='hello_task',
+    python_callable=print_hello,
+    dag=dag
+)
 
 hello_operator
